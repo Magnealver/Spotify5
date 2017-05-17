15d1057311045f8130d7b3760bca13ecac2b7516
Magnealver <magne.alver@gmail.com>
2017-05-17 19:43:55 +0200

    web api wrapper kekekek

--- app/src/main/java/leguin/spotify/MainActivity.java
+++ app/src/main/java/leguin/spotify/MainActivity.java
@@ -1,17 +1,16 @@
 package leguin.spotify;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.SettableFuture;
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
@@ -22,10 +21,13 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
+import com.wrapper.spotify.Api;
+import com.wrapper.spotify.models.AuthorizationCodeCredentials;
 
-import kaaes.spotify.webapi.android.*;
-import retrofit.Callback;
-import retrofit.http.GET;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
@@ -35,6 +37,7 @@ public class MainActivity extends Activity implements
     private static final String CLIENT_ID = "7ec97bdbb2434a779d97545c67cacbba";
     // TODO: Replace with your redirect URI
     private static final String REDIRECT_URI = "leguinapp://callback";
+    private static final String CLIENT_SECRET = "fe8533750fef4296bbd0d3e3f2cf1516";
 
     // Request code that will be used to verify if the result comes from correct activity
     // Can be any integer
@@ -46,25 +49,6 @@ public class MainActivity extends Activity implements
     RadioGroup radioGroup1;
     //RadioButton selected;
 
-  /*  SpotifyApi api = new SpotifyApi();
-
-    // Most (but not all) of the Spotify Web API endpoints require authorisation.
-// If you know you'll only use the ones that don't require authorisation you can skip this step
-    api.setAccessToken("myAccessToken");
-
-    SpotifyService spotify = api.getService();
-
-    spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
-        @Override
-        public void success(Album album, Response response) {
-            Log.d("Album success", album.name);
-        }
-
-        @Override
-        public void failure(RetrofitError error) {
-            Log.d("Album failure", error.toString());
-        }
-})*/
 
 
     @Override
@@ -124,13 +108,13 @@ public class MainActivity extends Activity implements
             }
         });
         //kod för inloggning via Spotifys api
-        /*  AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
                 AuthenticationResponse.Type.TOKEN,
                 REDIRECT_URI);
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
 
-        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);*/
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
     }
 
     @Override
@@ -209,6 +193,97 @@ public class MainActivity extends Activity implements
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
+    }
+
+    /*public void apiTest2(View view) {
+        SpotifyApi api = new SpotifyApi();
+
+        /*AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
+// Most (but not all) of the Spotify Web API endpoints require authorisation.
+// If you know you'll only use the ones that don't require authorisation you can skip this step
+        api.setAccessToken("test");
+
+        SpotifyService spotify = api.getService();
+
+        spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
+            @Override
+            public void success(Album album, Response response) {
+                Log.d("Album success", album.name);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Album failure", error.toString());
+            }
+        });
+    } */
+
+    public void apiTest(View view) {
+
+        final Api api = Api.builder()
+                .clientId(CLIENT_ID)
+                .clientSecret(CLIENT_SECRET)
+                .redirectURI(REDIRECT_URI)
+                .build();
+
+/* Set the necessary scopes that the application will need from the user */
+        final List<String> scopes = Arrays.asList("user-read-private", "user-read-email", "playlist-read-private", "playlist-modify-private", "user-library-read");
+
+/* Set a state. This is used to prevent cross site request forgeries. */
+        final String state = "leguinhype";
+
+        String authorizeURL = api.createAuthorizeURL(scopes, state);
+
+/* Continue by sending the user to the authorizeURL, which will look something like
+   https://accounts.spotify.com:443/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https://example.com/callback&scope=user-read-private%20user-read-email&state=some-state-of-my-choice
+ */
+        goToUrl(authorizeURL);
+
+
+    /* Application details necessary to get an access token */
+        Pattern pattern = Pattern.compile("(?<=code=)(.*)(?=&)");
+        Matcher matcher = pattern.matcher(authorizeURL);
+
+        final String code = "test";//matcher.group(1);
+
+    /* Make a token request. Asynchronous requests are made with the .getAsync method and synchronous requests
+     * are made with the .get method. This holds for all type of requests. */
+        final SettableFuture<AuthorizationCodeCredentials> authorizationCodeCredentialsFuture = api.authorizationCodeGrant(code).build().getAsync();
+
+/* Add callbacks to handle success and failure */
+        Futures.addCallback(authorizationCodeCredentialsFuture, new FutureCallback<AuthorizationCodeCredentials>() {
+            @Override
+            public void onSuccess(AuthorizationCodeCredentials authorizationCodeCredentials) {
+    /* The tokens were retrieved successfully! */
+                System.out.println("Successfully retrieved an access token! " + authorizationCodeCredentials.getAccessToken());
+                System.out.println("The access token expires in " + authorizationCodeCredentials.getExpiresIn() + " seconds");
+                System.out.println("Luckily, I can refresh it using this refresh token! " + authorizationCodeCredentials.getRefreshToken());
+
+    /* Set the access token and refresh token so that they are used whenever needed */
+                api.setAccessToken(authorizationCodeCredentials.getAccessToken());
+                api.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+    /* Let's say that the client id is invalid, or the code has been used more than once,
+     * the request will fail. Why it fails is written in the throwable's message. */
+                System.out.println("idk what the fuk went wrong ok" + throwable.getMessage());
+            }
+        });
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 /*
     public void sendMessage(View view) {
